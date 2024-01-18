fe2718e9be158e167348dec5a393e7ace55c9f00
Julio Romero <jromeroacive@gmail.com>
2024-01-18 04:07:51 -0430

    styles css

---
+++ views/contenido.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Contenido</title>
+
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">content</h1>
+        <a href="cerrar.php">Sign off</a>
+
+        <div class="content">
+        <article>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit enim provident officia. Odio, obcaecati pariatur quo quaerat laboriosam porro sunt, vel fugiat cum explicabo placeat excepturi quidem, exercitationem minima natus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptatibus natus accusantium odio quae distinctio pariatur possimus laudantium magni ipsa molestiae voluptates tenetur, dignissimos excepturi? Excepturi inventore ab laborum! Quisquam.</p>
+        </article>
+        </div>
+        
+    </div>
+</body>
+</html>
