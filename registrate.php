55365386312d00147395e7d30bd0bd03e84b1687
Julio Romero <jromeroacive@gmail.com>
2024-01-26 07:12:22 -0430

    Validating password

--- registrate.php
+++ registrate.php
@@ -37,15 +37,24 @@ if($resultado != false){
 }
 
 
+$password = hash('sha512', $password);
+$password2 = hash('sha512', $password2);
+
+if ($password != $password2){
+    $errores .= '<li>Las contraseñas no son iguales</li>';
 }
 
+}
+if($errores == ''){
+    $statement = $conexion->prepare('INSERT INTO usuarios (id, usuario, pass) VALUES (null, :usuario, :pass)');
+    $statement->execute(array(
+    ':usuario' => $usuario,
+    ':pass' => $password
+    ));
 
+    header('Location: login.php');
 }
 
-
-
-
+}
 require 'views/registrate.view.php';
-
-
 ?>
